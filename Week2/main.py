25868361e4aa032a27627fe24914287856194baa
Dmitrii Chernikov <dima.chernikov.053@mail.ru>
2025-02-19 10:56:14 +0300

    update main.py, README.md

--- Week2/main.py
+++ Week2/main.py
@@ -1,7 +1,58 @@
-class Themes:
+from abc import ABC, abstractmethod
+
+
+class AbstractThemes(ABC):
+    """
+    An abstract base class to represent a collection of themes.
+    """
+
+    @abstractmethod
+    def add_theme(self, value):
+        """
+        Add a new theme to the list of themes.
+
+        :param value: The theme to be added.
+        """
+        pass
+
+    @abstractmethod
+    def shift_one(self):
+        """
+        Move the last theme to the beginning of the list.
+        """
+        pass
+
+    @abstractmethod
+    def reverse_order(self):
+        """
+        Reverse the order of the themes.
+        """
+        pass
+
+    @abstractmethod
+    def get_themes(self):
+        """
+        Return the list of themes.
+
+        :return: The list of themes.
+        """
+        pass
+
+    @abstractmethod
+    def get_first(self):
+        """
+        Return the first theme in the list.
+
+        :return: The first theme.
+        """
+        pass
+
+
+class Themes(AbstractThemes):
     """
     A class to represent a collection of themes.
     """
+    __themes: list
 
     def __init__(self, *themes):
         """
@@ -48,18 +99,20 @@ class Themes:
         return self.__themes[0]
 
 
-class SpecialThemes(Themes):
+class PopularThemes(Themes):
     """
-    A class to represent a collection of special themes.
+    A class to represent a collection of popular themes.
     """
+    __popularity_rating: int
 
-    def __init__(self, *themes):
+    def __init__(self, *themes, rating):
         """
-        Initialize the SpecialThemes object with a list of themes.
+        Initialize the PopularThemes object with a list of themes.
 
         :param themes: A variable number of theme arguments.
         """
         super().__init__(*themes)
+        self.__popularity_rating = rating
 
     def add_theme(self, value):
         """
@@ -69,11 +122,64 @@ class SpecialThemes(Themes):
         """
         super().add_theme(value)
 
+    def get_popularity_rating(self):
+        """
+        Return the popularity rating of the themes.
 
-themes = Themes("weather", "rain")
-themes.add_theme("warm")
-print(themes.get_themes())
-themes.shift_one()
-print(themes.get_first())
-themes.reverse_order()
-print(themes.get_first())
+        :return: The popularity rating.
+        """
+        return self.__popularity_rating
+
+    def set_popularity_rating(self, rating):
+        """
+        Set the popularity rating of the themes.
+
+        :param rating: The popularity rating to be set.
+        """
+        self.__popularity_rating = rating
+
+
+class Discussion:
+    """
+    A class to represent a discussion.
+    """
+    themes: PopularThemes
+
+    def __init__(self, themes):
+        """
+        Initialize the Discussion object with a collection of themes.
+
+        :param themes: A collection of themes.
+        """
+        self.__themes = themes
+        self.__current_theme = self.__themes.get_first()
+
+    def get_current_theme(self):
+        """
+        Return the current theme of the discussion.
+
+        :return: The current theme.
+        """
+        return self.__current_theme
+
+    def change_theme(self):
+        """
+        Change the current theme of the discussion to the next theme.
+        """
+        self.__themes.shift_one()
+        self.__current_theme = self.__themes.get_first() 
+
+
+common_themes = Themes("weather", "rain")
+common_themes.add_theme("warm")
+print(common_themes.get_themes())
+common_themes.shift_one()
+print(common_themes.get_first())
+common_themes.reverse_order()
+print(common_themes.get_first())
+popular_themes = PopularThemes("Music", "Games", rating=4)
+print(popular_themes.get_first(), popular_themes.get_popularity_rating())
+discussion = Discussion(popular_themes)
+print(discussion.get_current_theme())
+discussion.change_theme()
+print(discussion.get_current_theme())
